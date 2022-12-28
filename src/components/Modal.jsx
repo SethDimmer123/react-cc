1d296897947ad625566b213746e88fd16a9c6274
Seth DImmer <sethdimmer1@gmail.com>
2022-12-28 13:40:37 -0500

    initial commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,11 @@
 import './Modal.css'
 
 function Modal() {
+  console.log()
     return ( 
 <div>
     <div className="modal">
-      <p className="modal__title">Are you sure?</p>
+      <p className="modal__title">Are Your sure?</p>
       <div className="modal__buttons">
         <button className="btn btn__cancel">Cancel</button>
         <button className="btn">Confirm</button>
