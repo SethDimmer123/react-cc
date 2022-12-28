ce2c89ffde81642146a6867dff11b38c8d2d0ff0
Seth DImmer <sethdimmer1@gmail.com>
2022-12-28 08:12:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo List</h1>
+      <div>
+        <h2>Finish the course</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Finish the interview section</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Land a 100K Job</h2>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
