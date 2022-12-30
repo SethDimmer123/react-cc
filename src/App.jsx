dfca5dd20d1ef181464ffef9f52cf3763b2b2b83
Seth DImmer <sethdimmer1@gmail.com>
2022-12-30 14:54:09 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import Modal from './components/Modal.jsx';
 import React, { useState } from 'react';
 
 function App() {
-  let isModalOpen = false;
 
-function toggleModal() {
-  isModalOpen = !isModalOpen
-  console.log(isModalOpen)
-}
+
 
   return (
     // the empty div tag is no use so that is the short hand
@@ -20,14 +16,14 @@ function toggleModal() {
         <input type="text" onChange={(event) => {
           console.log(event.target.value)
         }} />
-        <button onClick={toggleModal}>Add todo</button>
+        <button>Add todo</button>
       </div>
       <div className='todo__wrapper'>
       <Todo title ="Finish the course" />
       <Todo title ="Finish the Interview section"/>
       <Todo title ="Land 100k job"/>
       </div>
-      {isModalOpen ? <Modal title ="Confirm Delete?" /> : null}
+      {true ? <Modal title ="Confirm Delete?" /> : null}
 
     </>
   );
