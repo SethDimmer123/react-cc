4627bde29523119200dabb4a314680dadf9590f9
Seth DImmer <sethdimmer1@gmail.com>
2022-12-31 11:47:45 -0500

    initial commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,3 +1,22 @@
-function App() {
-    
+import React, { useState} from 'react';
+
+function Counter() {
+    const [Counter, setCounter] = useState(0)
+    function incrementCounter() {
+        setCounter(Counter + 1)
+
+    }
+
+    function decrementCounter() {
+        setCounter(Counter - 1)
+    }
+    return(
+        <div>
+            <button onClick={decrementCounter}>-</button>
+            {Counter}
+            <button onClick={incrementCounter}>+</button>
+        </div>
+    )
 }
+
+export default Counter
