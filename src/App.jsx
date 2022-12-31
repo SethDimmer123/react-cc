fa444cfb2bb755df0473292bc28317dfff276bb9
Seth DImmer <sethdimmer1@gmail.com>
2022-12-31 15:49:01 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
 
 
-// return <Counter />
+return <Counter />
 
 const [showModal, setShowModal] = useState(false)
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 
 function Counter() {
-    
+const [cart, setCart] = useState({
+    item:"apple",
+    quantity:0,
+});
+
+function addApple() {
+    setCart(prevCart => ({
+        ...prevCart,
+        quantity:1,
+    }))
+}
+
     return (
         <div>
-            <button>-</button>
-            <button>+</button>
+            <button onClick={decrementCounter}>-</button>
+            {counter}
+            <button onClick={incrementCounter}>+</button>
         </div>
     )
 }
