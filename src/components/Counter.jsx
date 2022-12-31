265a2a93875d55bdebd2fb8f7c23ab0d378f7a71
Seth DImmer <sethdimmer1@gmail.com>
2022-12-31 13:52:19 -0500

    initial commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,31 +1,42 @@
 import React, { useState } from 'react';
 
 function Counter() {
-    const [counter, setCounter] = useState(0)
-    function incrementCounter() {
-        setCounter((prevcounter) => prevcounter + 1)
-        setCounter((prevcounter) => prevcounter + 1)
+    const [cart, setCart] = useState({
+        item: 'apple', 
+        quantity: 0});
 
-    }
+        function removeApple() {
+//  1. use a callback to get the previous value
+//   2. spread out all the properties of the prev state
+//    3. only change the property that you need to change
+            setCart(prevCart => ({
+                ...prevCart,
+                quantity:prevCart.quantity - 1,
+            }))
+        }
 
-    // if i just want to change its value if the value
-    // depends on the previous value then i always
-    // want to use a callback function
-
-    function decrementCounter() {
-        setCounter((prevcounter) => prevcounter -1)
-        setCounter((prevcounter) => prevcounter -1)
-    }
-
-    // this is exactly how we update arrays and objects
+        function addApple() {
+            setCart(prevCart => ({
+                ...prevCart,
+                quantity:prevCart.quantity + 1,
+            }))
+        }
 
     return (
         <div>
-            <button onClick={decrementCounter}>-</button>
-            {counter}
-            <button onClick={incrementCounter}>+</button>
+            <button onClick={removeApple}>-</button>
+            {cart.quantity}
+            {cart.item}
+            <button onClick={addApple}>+</button>
         </div>
     )
 }
 
 export default Counter
+
+
+// we are shopping and we had a cart
+
+// are quantity of the cart is 0 
+
+// but we wanted to buy 2 apples
