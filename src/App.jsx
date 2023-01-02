b6e10aecfce023f27af84f8297ce24570edb7065
Seth DImmer <sethdimmer1@gmail.com>
2023-01-02 09:47:44 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 // import Modal from './components/Modal.jsx';
 // import Counter from "./components/Counter.jsx"
 // import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route, Link } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -16,9 +16,9 @@ function App() {
     <div>
       <Router>
         <nav>
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
