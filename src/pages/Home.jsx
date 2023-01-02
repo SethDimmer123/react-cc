4dab4cdca847af29c2fcf164294c536ed7807352
Seth DImmer <sethdimmer1@gmail.com>
2023-01-02 14:00:18 -0500

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import axios from 'axios'
 
 function Home() {
+    const [users, setUsers] = useState([])
+
     async function fetchUsers () {
     const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
-    console.log(data)
+    setUsers(data)
     }
 
 useEffect(() => {
 fetchUsers();
 }, []);
-return <h1>Home</h1>
+
+return <h1>{users.length > 0 ? users[0].name :null}</h1>
 }
 
 export default Home
