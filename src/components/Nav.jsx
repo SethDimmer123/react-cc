ea6521aa2f09d912be439eb6c97281a92b51d0ee
Seth DImmer <sethdimmer1@gmail.com>
2023-01-02 11:00:38 -0500

    initial commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,8 +5,9 @@ function Nav() {
     return(
         <nav>
         <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/users/ronaldo">Ronaldo</Link>
+        <Link to="/users/messi">Messi</Link>
+        <Link to="/users/br4gg">Seth</Link>
       </nav>
     );
 
