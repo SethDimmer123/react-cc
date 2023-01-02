812035c6dbeaa0614b36ba943f571c72433d702b
Seth DImmer <sethdimmer1@gmail.com>
2023-01-02 11:04:36 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Home from './pages/Home.jsx';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Nav from './components/Nav';
-
+import Users from "./pages/Users";
 
 
 function App() {
@@ -19,8 +19,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/users/:name" element={<Users />} />
         </Routes>
       </Router>
     // </div>
